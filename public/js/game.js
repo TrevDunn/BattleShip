fe6c7919dc5865e0898ce02b5e0952f7746e8ab7
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-12-15 15:51:37 -0500

    game logic for ship placement completed in module; links to back-end remain

--- public/js/game.js
+++ public/js/game.js
@@ -40,7 +40,7 @@ let GameModule = (function(){
 		},
 
 		// check all ships to see if remaining in bounds
-		boundaryCheck: function(newShip){
+		checkBounds: function(newShip){
 			if (newShip.ori_x && (10-newShip.bow_x) <= newShip.length) {
 				return true;
 			} else if (!newShip.ori_x && (10-newShip.bow_y) <= newShip.length) {
@@ -51,13 +51,16 @@ let GameModule = (function(){
 		},
 
 		// fill in usedCoords array
-		fillCoords: function(newShip.length, newShip.ori_x, newShip.bow_x, newShip.bow_y){
+		fillCoords: function(newShip){
 			for (let i = 0; i < newShip.length; i++) {
-				// if oriented on X-axis
+				// if ship on X-axis
 				if (newShip.ori_x) {
-					let newBowCoords = { xc: (i + newShip.bow_x), yc: newShip.bow_y };
+					let newBowCoords = { (i + newShip.bow_x): newShip.bow_y };
+					usedCoords.push(newBowCoords);
+				// if ship on Y-axis
 				} else if (!newShip.ori_x) {
-					let newBowCoords = { xc:  newShip.bow_x, yc: (i + newShip.bow_y) };
+					let newBowCoords = { newShip.bow_x: (i + newShip.bow_y) };
+					usedCoords.push(newBowCoords);
 				} else {
 					console.log('something went wrong in fillCoords');
 				}
@@ -65,8 +68,26 @@ let GameModule = (function(){
 		},
 
 		// check to see if ship overlaps others
+		checkOverlap: function(newShip) {
+			let xAxis = newShip.bow_x;
+			let yAxis = newShip.bow_y;
 
+			// check ship on the X-axis
+			for (let i = 0; i < newShip.length; i++) {
+				if (newShip.ori_x && usedCoords[i].xAxis == !undefined) {
+					console.log(usedCoords[i].xAxis);
+					return false;
+				}
+			}
 
+			// check ship on the Y-axis
+			for (let i = 0; i < newShip.length; i++) {
+				if (!newShip.ori_x && usedCoords[i].yAxis == !undefined) {
+					console.log(usedCoords[i].xAxis);
+					return false;
+				}
+			}
+		},
 
 		// check to see if ship
 
