299b836c4cf514cd18ead8caee27c27e4353404d
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-12-16 18:50:29 -0500

    ship placement  options function

--- public/js/gameController.js
+++ public/js/gameController.js
@@ -25,12 +25,20 @@ function GameController($http) {
 	self.oceanArray = [];
 	self.showGameBoard = false;
 	self.playerArray = [];
-	self.test = 'This is a good test';
 	self.newShip = {};
+	// self.test = 'This is a good test';
+	self.test = function(tile){
+		self.oceanArray[1][0].occupied = true;
+		console.log(self.oceanArray[0]);
+		console.log('Successful test');
+		console.log(tile);
+	}
+
 
 	// ship placement
 	self.tempShipID = undefined;
 	self.tempShipLength = undefined;
+	self.shipPlacing = false;
 
 	// ship object list
 	self.shipList = [
@@ -74,6 +82,8 @@ function GameController($http) {
 	// function links
 	self.clickShipModel = clickShipModel;
 	self.setShipBow = setShipBow;
+	// self.occupiedCheckX = occupiedCheckX;
+	// self.occupiedCheckY = occupiedCheckY;
 
 	// function calls
 	generateBoardArray();
@@ -81,12 +91,12 @@ function GameController($http) {
 
 	// creates board-array
 	function generateBoardArray() {
-		for (let x = 0; x < 10; x++) {
+		for (let y = 0; y < 10; y++) {
 			self.oceanArray.push([]);
-			for (let y = 0; y < 10; y++) {
-				self.oceanArray[x].push({
-					'xlat': x+1,
-				 	'ylon': y+1,
+			for (let x = 0; x < 10; x++) {
+				self.oceanArray[y].push({
+				 	'ylon': y,
+					'xlat': x,
 					clickable: true,
 					occupied: false,
 					option: false,
@@ -115,32 +125,69 @@ function GameController($http) {
 	function clickShipModel(ship) {
 		self.tempShipID = ship.id;
 		self.tempShipLength = ship.length;
+		ship.clickable = false;
 		console.log('Hit: clickShipModel; tempShipID is ' + self.tempShipID);
 	}
 
 	// function to set ship-setting state; turns off all other click events
 	function setShipBow(tile) {
+		console.log("Hit: setShipBow");
 
-		if ((tile.xlat + self.tempShipLength) < 10) {
-			for (let i = tile.xlat-1; i < (tile.xlat+self.tempShipLeng-1); i++) {
-				if (true) {
+		let optionX = (tile.xlat + self.tempShipLength - 1);
+		let optionY = (tile.ylon + self.tempShipLength - 1);
+		let placableShip = false;
 
-				}[i]
-			}
+
+		// change class of option X tile to 'option'
+		if (occupiedCheckX(tile, optionX)) {
+			self.oceanArray[tile.ylon][optionX].option = true;
+			placableShip = true;
+			console.log('optionX is '+optionX);
+		} else {
+			console.log("No optionX");
 		}
 
-		document.querySelector
+		// change class of option X tile to 'option'
+		if (occupiedCheckY(tile, optionY)) {
+			self.oceanArray[optionY][tile.xlat].option = true;
+			placableShip = true;
+			console.log('optionY is '+optionY);
+		} else {
+			console.log("No optionY");
+		}
+
 		// change class of clicked tile to 'occupied'
+		if (placableShip) {
 			tile.occupied = true;
-		// change class of option tiles to 'option'
-
-		//
-		// self.oceanArray
-		// tile.xlat
-		// tile.ylon
-		console.log(this);
-
+			self.shipPlacing = true;
+			self.tempShipID = undefined;
+			self.tempShipLength = undefined;
+		}
 	}
+
+	// function to check if any space is occupied for options (x)
+	function occupiedCheckX(tile, optionX) {
+		for (let i = 0; i < optionX-tile.xlat; i++) {
+			if ((optionX >= 10) || self.oceanArray[tile.ylon][tile.xlat + i].occupied) {
+				return false
+				break
+			}
+		}
+		return true;
+	}
+
+	// function to check if any space is occupied for options (y)
+	function occupiedCheckY(tile, optionY) {
+		for (let i = 0; i < optionY-tile.ylon; i++) {
+			if ((optionY >= 10) || self.oceanArray[tile.ylon + i][tile.xlat].occupied) {
+				return false
+				break
+			}
+		}
+		return true;
+	}
+
+	function 
 
 	// function to activate viable down and right boxes for ship placement
 
