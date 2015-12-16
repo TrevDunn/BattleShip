33378c66ea5a86e3036c5e25a17ed25115d8ade8
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-12-16 15:23:13 -0500

    adding classes to tables based upon x and y coords

--- public/js/gameController.js
+++ public/js/gameController.js
@@ -22,14 +22,15 @@ function GameController($http) {
 
 	// controller variable setup
 	let self = this;
-	self.array = [];
+	self.oceanArray = [];
 	self.showGameBoard = false;
 	self.playerArray = [];
 	self.test = 'This is a good test';
 	self.newShip = {};
 
 	// ship placement
-	self.tempShip = undefined;
+	self.tempShipID = undefined;
+	self.tempShipLength = undefined;
 
 	// ship object list
 	self.shipList = [
@@ -72,6 +73,7 @@ function GameController($http) {
 
 	// function links
 	self.clickShipModel = clickShipModel;
+	self.setShipBow = setShipBow;
 
 	// function calls
 	generateBoardArray();
@@ -80,20 +82,21 @@ function GameController($http) {
 	// creates board-array
 	function generateBoardArray() {
 		for (let x = 0; x < 10; x++) {
-			self.array.push([]);
+			self.oceanArray.push([]);
 			for (let y = 0; y < 10; y++) {
-				self.array[x].push({
-					'x-lat': x+1,
-				 	'y-lon': y+1,
+				self.oceanArray[x].push({
+					'xlat': x+1,
+				 	'ylon': y+1,
 					clickable: true,
 					occupied: false,
+					option: false,
 					hit: false,
 					miss: false,
 				});
 
 			}
 		}
-		console.log(self.array);
+		console.log(self.oceanArray);
 	}
 
 	// creates ship-array
@@ -101,8 +104,8 @@ function GameController($http) {
 		for (let i = 0; i < self.shipList.length; i++) {
 			for (let f = 0; f < self.shipList[i].length; f++) {
 				self.shipList[i].shipContains.push({
-					'x-lat': undefined,
-					'y-lon': undefined,
+					'xlat': undefined,
+					'ylon': undefined,
 				});
 			}
 		}
@@ -110,10 +113,38 @@ function GameController($http) {
 
 	// function to pass ship ID to square to initialize placement
 	function clickShipModel(ship) {
-		self.tempShip = ship.id;
-		console.log('Hit: clickShipModel; tempShip is ' + self.tempShip);
+		self.tempShipID = ship.id;
+		self.tempShipLength = ship.length;
+		console.log('Hit: clickShipModel; tempShipID is ' + self.tempShipID);
 	}
 
+	// function to set ship-setting state; turns off all other click events
+	function setShipBow(tile) {
+
+		if ((tile.xlat + self.tempShipLength) < 10) {
+			for (let i = tile.xlat-1; i < (tile.xlat+self.tempShipLeng-1); i++) {
+				if (true) {
+
+				}[i]
+			}
+		}
+
+		document.querySelector
+		// change class of clicked tile to 'occupied'
+			tile.occupied = true;
+		// change class of option tiles to 'option'
+
+		//
+		// self.oceanArray
+		// tile.xlat
+		// tile.ylon
+		console.log(this);
+
+	}
+
+	// function to activate viable down and right boxes for ship placement
+
+	// function to re-activate all tiles as clickable except those with ships
 
 
 	// check if tile is placable at stern (down and right only)
@@ -124,7 +155,7 @@ function GameController($http) {
 }
 
 
-// !!!
+// fff
 
 
 
